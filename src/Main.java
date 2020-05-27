ad2848707542e67c646f268226d90d25039056b5
Serdar <serdar2225@gmail.com>
2020-05-27 00:43:48 -0400

    project 8 first commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class Main {
 
     /*
-    Navigate to https://www.phptravels.net/en	phpTraverlers home page should be displayed
-Click on language dropdown 	Dropdown should be expand and different language options should be displayed
-Click on each language and verify language is changed in the website 	Language of the website should be changed
+    Navigate to https://www.phptravels.net/en
+    phpTraverlers home page should be displayed
+    Click on language dropdown
+    Dropdown should be expand and different language options should be displayed
+    Click on each language and verify language is changed in the website
+    Language of the website should be changed
 
      */
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\suler\\Desktop\\Selenium\\chromedriver\\chromedriver.exe");
 
@@ -19,6 +26,19 @@ Click on each language and verify language is changed in the website 	Language o
 
         driver.manage().window().maximize();
 
+        WebElement dropDown = driver.findElement(By.xpath("//a[@id='dropdownLangauge']"));
+        dropDown.click();
 
+        int size = driver.findElements(By.xpath("//a[@class='dropdown-item']")).size();
+        System.out.println(size);
+
+        List<WebElement> languages = driver.findElements(By.xpath("//div[@class='dropdown dropdown-language show']//a"));
+        for (int i = 0; i <languages.size() ; i++) {
+            if(languages.get(i).getText().equalsIgnoreCase("English")) {
+                System.out.println(languages.get(i).getText());
+            }
+        }
+
+            driver.quit();
     }
 }
